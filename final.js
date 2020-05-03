53c3336c7332609f2aa70c2e4dff5b2d1a1dd175
ForTheOnesThere <jamescockayne1@gmail.com>
2020-05-03 17:26:08 +0100

    Added another graph to the results page.

--- final.js
+++ final.js
@@ -7,15 +7,20 @@ let thirdYearMark = Number(window.localStorage.yearThreeMark);
 //hides the labels on all of the graphs
 Chart.defaults.global.legend.display = false;
 
+//clear the data from localStorage
+//window.localStorage.clear();
+let canvasPie = document.getElementById('pieChartContainer');
+canvasPie.height = window.innerHeight * 0.5;
+canvasPie.width = window.innerWidth;
 
-window.localStorage.clear();
-let canvas = document.getElementById('chartContainer');
-canvas.height = window.innerHeight * 0.5;
-canvas.width = window.innerWidth;
+let canvasBar = document.getElementById('barChartContainer');
+canvasBar.height = window.innerHeight * 0.5;
+canvasBar.width = window.innerWidth;
 
 var lambda = null;
 let finalMark = null;
 let classification = null;
+let result = null;
 
 function findSafetyMark(){
     if (2*A > 120){lambda=1} else {lambda = (2*A/120).toFixed(1)}
@@ -25,7 +30,7 @@ function findSafetyMark(){
 }
 
 function findSafetyClassification(){
-  let result = (secondYearMark*0.2)+(thirdYearMark*0.4)+(finalMark*0.4);
+  result = (secondYearMark*0.2)+(thirdYearMark*0.4)+(finalMark*0.4);
 
   if (50<=result && result<60){classification = '2:2'}
   else if (60<=result && result<70) {classification = '2:1'}
@@ -43,8 +48,17 @@ document.querySelectorAll('div')[0].innerHTML = "<h2>" + finalMarkRounded + "</h
 document.querySelectorAll('div')[1].innerHTML = "<h2>" + classification + "</h2>"
 
 // Using chart.js
-var ctx = document.getElementById('chartContainer');
-let data = {
+// defines the axis range for the bar chart
+Chart.scaleService.updateScaleDefaults('linear', {
+    ticks: {
+        min: 0,
+        max: 100
+    }
+});
+
+//adds a pie chart
+var ctx = document.getElementById('pieChartContainer');
+let dataPie = {
     datasets: [{
         data: contributions,
         backgroundColor: ['rgba(188, 141, 160, 1)','rgba(173, 188, 165, 1)']
@@ -56,5 +70,22 @@ let data = {
 
 var myDoughnutChart = new Chart(ctx, {
     type: 'doughnut',
-    data: data,
+    data: dataPie,
+});
+
+//adds a bar chart
+let dataBar = {
+      labels: ["Final Overall Grade"],
+      datasets: [
+        {
+          data: [result],
+          backgroundColor: ['rgba(204,139,134,1)']
+        }
+      ]
+    };
+
+var ctxBar = document.getElementById('barChartContainer');
+var myBarChart = new Chart(ctxBar, {
+    type: 'bar',
+    data: dataBar
 });
